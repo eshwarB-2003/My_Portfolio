363d163dcbcc3a6a8e5a9d02a3807b548e5b7e01
eshwarB-2003 <e8082409@gmail.com>
2025-10-05 23:43:58 +0100

    Rebuild dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/My_Portfolio/',
+   build: {
+    outDir: 'dist', // 👈 ensures output is in 'dist'
+  },
 });
 
