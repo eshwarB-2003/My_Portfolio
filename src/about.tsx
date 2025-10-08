f2ed7b35d956af53a5cb31fbc8e33e05717882d6
eshwarB-2003 <e8082409@gmail.com>
2025-10-08 13:34:55 +0100

    updated portfolio

--- src/about.tsx
+++ src/about.tsx
@@ -1,55 +1,44 @@
-import React from "react";
-import './about.css';
-import ReactTypingEffect from "react-typing-effect";
-import profileImg from "./assets/img.jpg";
+import {useState} from "react";
+import reactLogo from "./assets/img.jpg"; //  import image
+import "./navbar.css";
 
-const About: React.FC = () => {
-return (
-    <section id="about" className="about-section">
-        {/*Background Shape*/}
-         <div className="about-bg-shape"></div>
+function Navbar() {
+    // state to track dark node
+    const[darkMode, setDarkMode] = useState(false);
 
-         <div className="about-content">
-            <div className="profile-container">
-            <img src= {profileImg} alt="Vaitheeshwar" className="profile-pic"/>
-         </div>
-         <div className="text-container">
-            <h1 className ="typing-text">
-                <ReactTypingEffect
-                text={["Hi, this is Vaitheeshwar Badrinarayanan!",
-                "Welcome to my portfolio"
-              ]}
-              speed={100}
-              eraseSpeed={80}
-              typingDelay={500}
-              eraseDelay={1500}
-              cursorRenderer={(cursor:string)=> <span style={{color:"orange"}}>{cursor}</span>}
-              displayTextRenderer={(text: string) => (<span className="typing-gradient glow">{text}</span>)}
-            />
-            </h1>
-             <p className="about-intro">
-              <ReactTypingEffect
-                text={[" Pursuing my Masters in Software Engineering, Student @ University of Limerick, Ireland"
-              ]}
-              speed={100}
-              eraseSpeed={80}
-              typingDelay={500}
-              eraseDelay={1500}
-              cursorRenderer={(cursor:string)=> <span style={{color:"orange"}}>{cursor}</span>}
-              displayTextRenderer={(text: string) => (<span className="typing-gradient glow">{text}</span>)}
-            />
-            
-          </p>
-          <p className="about-description">
-            I am a passionate Software Engineer with a strong foundation in Computer Science 
-            and hands-on experience in full-stack web development. Currently pursuing my 
-            Master’s at the University of Limerick, I thrive on solving complex problems 
-            through code and building solutions that enhance user experience and efficiency.
-          </p>
-          <a href= "https://drive.google.com/file/d/1xJex43uwHhq_Uk4t5Jw3muIp4RLu4s6H/view?usp=sharing" download className="resume-btn">Download Resume</a>
-         </div>
-         </div>
-    </section>
-);
+    // toggle Function where if user clicks the toggle button it changes to white bg mode 
+    const toggleDarkMode = () => {
+  setDarkMode(!darkMode);
+  if (!darkMode) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+};
+
+  return (
+   <header className={`header ${darkMode ? "dark" : "light"}`}>
+      <nav className="navbar">
+        <div className="logo-container">
+          <img src={reactLogo} alt="React-logo" className="logo react-logo" />
+        </div>
+        <ul className="nav-links">
+          <li><a href="#about">About</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#certificates">Certifications</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        {/*  Dark/Light- Toggle Button  */}
+        <button onClick={toggleDarkMode} className ="toggle-btn"> {/* Switches from dark mode to Light mode using ternary logic*/}
+            {darkMode ?    "Light Mode" : " Dark Mode"}
+        </button>
+      </nav>
+    </header>
+  );
 }
-export default About;
+
+export default Navbar;
