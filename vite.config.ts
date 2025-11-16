7c9c9f8eadcc222494d80ac0e009faaec1cf6d3c
eshwarB-2003 <e8082409@gmail.com>
2025-11-16 20:46:50 +0000

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 //https://vite.dev/config
 export default defineConfig({
   plugins: [react()],
-  base: "/My_Portfolio/",  // MUST MATCH your repo name
+  base: '/My_Portfolio/',  // MUST MATCH your repo name
   build: {
-    outDir: "dist",         // GitHub Pages uses docs folder
+    outDir: 'dist',         // GitHub Pages uses docs folder
   },
 });
