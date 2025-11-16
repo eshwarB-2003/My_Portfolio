734579c5a3365af3333e04cdfd759d8d8b49055c
eshwarB-2003 <e8082409@gmail.com>
2025-11-16 21:01:27 +0000

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-//https://vite.dev/config
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/My_Portfolio/',  // MUST MATCH your repo name
-  build: {
+ /* build: {
     outDir: 'dist',         // GitHub Pages uses docs folder
   },
+  */
 });
